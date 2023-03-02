c4ecc70e837e3e48c9174d1861ebca95574b9cef
Chris Weber <johndoe@example.com>
2023-03-02 11:59:28 +0100

    first milestone reached

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log('Hello World!');
+
+
+class Player {
+    constructor(){
+        this.positionX = 0;
+        this.positionY = 0;
+        this.playerElm = document.getElementById("player");
+    }
+    
+    moveLeft(){
+        this.positionX--;
+        this.playerElm.style.left = this.positionX + "vw";
+        console.log(`current postion...`, this.positionX)
+    }
+    moveRight(){
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + "vw";
+        console.log(`current postion...`, this.positionX)
+    }
+}
+
+
+
+const myPlayer = new Player();
+
+// attach event listeners
+
+document.addEventListener("keydown",(e) => {
+    if(e.key === "ArrowLeft"){
+        myPlayer.moveLeft();
+    }else if (e.key === "ArrowRight"){
+        myPlayer.moveRight();
+    }
+});
